0b62b1b243c1e6157d1fe98433c011f9b11339b0
joaodunas <joaomariadonato@gmail.com>
2022-04-23 19:07:42 +0100

    km

--- AED/4/4.1/4.2.py
+++ AED/4/4.1/4.2.py
@@ -8,18 +8,12 @@ def readInput():
 
 def percentil(matrix, value):
     n = 0
-    lenMat = 0
-    if isinstance(matrix[0], list):
-        lenMat = len(matrix) * len(matrix[0])
-        for i in matrix:
-            for l in i:
-                if l < value:
-                    n += 1
-    else:
-        lenMat = len(matrix)
-        for i in matrix:
-            if i < value:
-                n += 1
+
+    lenMat = len(matrix)
+    i = 0
+    while i < lenMat and matrix[i] < value:
+        n += 1
+        i += 1
     val = int(n / lenMat * 100)
     return str(val) + " "
 
@@ -53,38 +47,42 @@ def main():
             else:
                 N = int(inp[1])
                 M = int(inp[2])
-                raster = [[0 for x in range(M)] for x in range(N)]
                 rasterUni = []
-                h = 0
                 for i in range(N):
                     inp2 = readInput()
                     if len(inp2) != M:
                         stdout.write("WRONG FORMAT\n")
                         return 1
                     else:
-                        if M > 1:
-                            for l in range(M):
-                                raster[i][l] = int(inp2[l])
-                                rasterUni.append(int(inp2[l]))
-                                h += 1
-                        else:
-                            raster[i] = int(inp2[0])
-                            rasterUni.append(int(inp2[0]))
-                            h += 1
+                        for l in range(M):
+                            rasterUni.append(int(inp2[l]))
+
+                #bubble sort
+
+                for i in range(len(rasterUni)):
+                    h = len(rasterUni)-1-i
+                    c = 1
+
+                    while c <= h:
+                        if rasterUni[c] < rasterUni[c-1]:
+                            buffer = rasterUni[c]
+                            rasterUni[c] = rasterUni[c-1]
+                            rasterUni[c-1] = buffer
+                        c += 1
 
                 stdout.write("RASTER GUARDADO\n")
+
+
         if inp[0] == "AMPLITUDE":
-            maximum = rasterUni[0]
-            minimum = rasterUni[0]
-            for i in rasterUni:
-                if maximum < i:
-                    maximum = i
-                if minimum > i:
-                    minimum = i
-            stdout.write(str(maximum - minimum) + "\n")
+            stdout.write(str(rasterUni[-1] - rasterUni[0]) + "\n")
 
         if inp[0] == "MEDIANA":
-            stdout.write(str(mediana(rasterUni)) + "\n")
+            if len(rasterUni) % 2 == 0:
+                res = (rasterUni[int(len(rasterUni) / 2) - 1] + rasterUni[
+                    int(len(rasterUni) / 2)]) / 2
+                stdout.write(str(int(res)) + "\n")
+            else:
+                stdout.write(str(rasterUni[int(len(rasterUni) / 2)]) + "\n")
 
         if inp[0] == "PERCENTIL":
             if len(inp) != 2:
@@ -97,12 +95,11 @@ def main():
                     stdout.write("WRONG NUMBER OF NUMBERS\n")
                 else:
                     for i in range(len(inp2)):
-                        payload += percentil(raster, int(inp2[i]))
+                        payload += percentil(rasterUni, int(inp2[i]))
 
                     stdout.write(payload.rstrip())
                     stdout.write("\n")
         inp = readInput()
-
 
 
 if __name__ == '__main__':
